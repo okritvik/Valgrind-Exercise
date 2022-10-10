1d62cd015e9b0cb377c44c29bdae275389409692
okritvik <okritvik@gmail.com>
2022-10-10 13:42:18 -0400

    Added results and modified readme

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <AnalogSensor.hpp>
+#include "../include/AnalogSensor.hpp"
 
 int main()
 {
     AnalogSensor lightSensor(5);
     std::cout << "Averaged sensor reading: " << lightSensor.Read() << std::endl;
-    bool terminator;
+    bool terminator = true;
     if( terminator )
     {
         std::cout << "DONE" << std::endl;
